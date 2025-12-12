ed8497cef6ef0d02acbdb07696ad7ceba4af4ff9
Thanatcha006 <151045724+Thanatcha006@users.noreply.github.com>
2025-12-13 02:12:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from streamlit_drawable_canvas import st_canvas
 st.set_page_config(page_title="Parkinson Tester", layout="centered")
 
 st.title("🧠 Parkinson Detection")
+st.title("----------")
 
 st.divider()
 
