b989bffd209002f0e920dc7450fb066e9d532c7d
Aqueouzx <hibarikung1395@gmail.com>
2025-12-14 10:38:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ st.markdown(f"""
     <a href="#test_area" class="cta-button">เริ่มทำแบบทดสอบ ➝</a>
 </div>
 """, unsafe_allow_html=True)
-st.image("parkinson cover.png", width=1000, height=1500)
+st.image("parkinson cover.png", width=100%)
 
 # ----------------------------------
 # 6. Model & Logic
